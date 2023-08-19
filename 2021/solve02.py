7e0f21b1ce27785b2f146e5c05b23eb3e1d10a0d
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2023-08-19 01:58:27 +0100

    Improve the type annotations for 2021 day 2.

--- 2021/solve02.py
+++ 2021/solve02.py
@@ -4,6 +4,8 @@ Part 1: 2187380
 Part 2: 2086357770
 """
 
+from typing import Literal, TypeAlias
+
 import argparse
 import collections
 import functools
@@ -12,45 +14,47 @@ import pathlib
 parser = argparse.ArgumentParser(prog='Dive!')
 parser.add_argument('datafile', type=pathlib.Path)
 
+Opcode: TypeAlias = Literal['forward', 'down', 'up']
+
+Cmd = collections.namedtuple('Cmd', ['opcode', 'value'])
 Pos = collections.namedtuple('Pos', ['shift', 'depth'])
-
-
-def apply(pos: Pos, command: str) -> Pos:
-    opcode: str = command[0]
-    offset: int = int(command[1])
-    match opcode:
-        case 'forward':
-            return Pos(shift=pos.shift + offset, depth=pos.depth)
-        case 'down':
-            return Pos(shift=pos.shift, depth=pos.depth + offset)
-        case 'up':
-            return Pos(shift=pos.shift, depth=pos.depth - offset)
-
-
 Pos3 = collections.namedtuple('Pos3', ['shift', 'depth', 'aim'])
 
 
-def apply3(p: Pos3, command: str) -> Pos3:
-    opcode: str = command[0]
-    offset: int = int(command[1])
-    match opcode:
+def parse(line: str) -> Cmd:
+    (opcode, valuestr) = line.strip().split()
+    return Cmd(opcode=opcode, value=int(valuestr))
+
+
+def apply(pos: Pos, cmd: Cmd) -> Pos:
+    match cmd.opcode:
         case 'forward':
-            diff = p.aim * offset
-            return Pos3(shift=p.shift + offset,
-                        depth=p.depth + diff,
-                        aim=p.aim)
+            return Pos(shift=pos.shift + cmd.value, depth=pos.depth)
         case 'down':
-            return Pos3(shift=p.shift, depth=p.depth, aim=p.aim + offset)
+            return Pos(shift=pos.shift, depth=pos.depth + cmd.value)
         case 'up':
-            return Pos3(shift=p.shift, depth=p.depth, aim=p.aim - offset)
+            return Pos(shift=pos.shift, depth=pos.depth - cmd.value)
+
+
+def apply3(p: Pos3, cmd: Cmd) -> Pos3:
+    match cmd.opcode:
+        case 'forward':
+            move = cmd.value
+            fall = p.aim * cmd.value
+            return Pos3(shift=p.shift + move, depth=p.depth + fall, aim=p.aim)
+        case 'down':
+            return Pos3(shift=p.shift, depth=p.depth, aim=p.aim + cmd.value)
+        case 'up':
+            return Pos3(shift=p.shift, depth=p.depth, aim=p.aim - cmd.value)
 
 
 def main(args):
     with open(args.datafile) as datafile:
-        fullcommands = [tuple(line.strip().split(' ')) for line in datafile]
-    part1: Pos = functools.reduce(apply, fullcommands, Pos(0, 0))
+        commands = [parse(line) for line in datafile]
+    part1: Pos = functools.reduce(apply, commands, Pos(0, 0))
     print("Part 1: {ans}".format(ans=part1.shift * part1.depth))
-    part2: Pos3 = functools.reduce(apply3, fullcommands, Pos3(0, 0, 0))
+
+    part2: Pos3 = functools.reduce(apply3, commands, Pos3(0, 0, 0))
     print("Part 2: {ans}".format(ans=part2.shift * part2.depth))
 
 
