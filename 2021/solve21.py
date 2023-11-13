f6a7ba3593369be717cead048d85668870659d78
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2023-11-13 19:09:43 +0530

    aoc 2021: Add solution for problem 21 - part1.

---
+++ 2021/solve21.py
@@ -0,0 +1,67 @@
+"""Solution - Dirac Dice.
+
+Link: https://adventofcode.com/2021/day/21 
+Part 1: 513936
+Part 2: ???
+"""
+
+import argparse
+import dataclasses
+import itertools
+import pathlib
+
+import more_itertools
+
+parser = argparse.ArgumentParser()
+parser.add_argument('datafile', type=pathlib.Path)
+
+
+def parse(datafile: str):
+    with open(datafile) as f:
+        return tuple([int(line.split(':')[-1].strip()) for line in f])
+        
+
+def take(n: int, iterable) -> list:
+    """Return first n items of the iterable as a list."""
+    return list(itertools.islice(iterable, n))
+
+
+@dataclasses.dataclass
+class Player:
+    score: int
+    position: int 
+
+def move(player: Player, steps: int):
+    player.position = ((player.position + steps - 1) % 10) + 1
+    player.score += player.position
+
+
+def is_winner(player: Player) -> bool:
+    return player.score >= 1000
+
+def throw(rolls3 : list[tuple[int, int]]) -> int:
+    steps = [roll[-1] for roll in rolls3]
+    throws = max(roll[0] for roll in rolls3)
+    return (sum(steps), throws)
+
+def simulate(a, b):
+    dice = enumerate(itertools.count(1), start=1)
+    dice3 = more_itertools.chunked(dice, 3)
+    for curr, other in itertools.cycle(((a, b), (b, a))):
+        if not is_winner(curr):
+            steps, throws = throw(next(dice3))
+            move(player=curr, steps=steps)
+            if is_winner(curr):
+                return throws * other.score
+
+
+def main(args):
+    pos1, pos2 = parse(args.datafile)
+    p1 = Player(score=0, position=pos1)
+    p2 = Player(score=0, position=pos2)
+    answer = simulate(p1, p2)
+    print(f'Part 1: {answer}')
+
+
+if __name__ == '__main__':
+    main(args=parser.parse_args())
